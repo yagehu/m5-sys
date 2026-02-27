f80e1e0d2472ca8c0ce52460ac5efb4aaae25939
Yage Hu <me@huyage.dev>
2026-02-27 15:04:26 -0500

    Make dependency discovery more flexible

--- README.md
+++ README.md
@@ -3,11 +3,22 @@
 Low-level Rust bindings to the m5 utility library of the
 [gem5](https://www.gem5.org/) architectural simulator.
 
-## Finding gem5 source
+## Native Dependency
 
-There are two ways for this crate to link against m5.
+There are a few ways for this crate to link against m5.
 
-- By default, this crate will download the official gem5 release
-  from GitHub and build the m5 library.
-- Set the `GEM5_SRC` environment variable to the root of the gem5
-  source tree to build from that instead.
+- If environment variable `GEM5_M5_LIB_DIR` is set, this crate will look for
+  a compiled m5 library under said path.
+  - If environment variable `GEM5_M5_INCLUDE` is set, this crate will add it
+    to the include search path of bindgen.
+  - Otherwise, this crate will guess the include path is
+    `${GEM5_M5_LIB_DIR}/../include`.
+- Else, if `GEM5_M5_BUILD` is unset,
+  this crate will make no effort to search for m5 other than the
+  system default of `/usr/lib` and `/usr/include`.
+- Else, if `GEM5_SRC` is set,
+  this crate will try to build m5 using the source at said path and use this
+  directory for the include path of bindgen.
+- Else, if `GEM5_SRC` is unset,
+  this crate will download a release tarball of gem5 source code and attempt
+  to build m5 from it.
--- build.rs
+++ build.rs
@@ -24,117 +24,154 @@ impl CallType {
     }
 }
 
+static GEM5_VERSION: &str = "25.1.0.1";
+
 fn main() {
     println!("cargo:rustc-link-lib=static=m5");
+    println!("cargo:rerun-if-changed=wrapper.h");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let gem5_dir = match env::var("GEM5_SRC") {
-        Ok(path) => PathBuf::from(path),
+    let include = match env::var("GEM5_M5_LIB_DIR") {
+        Ok(m5_lib_dir) => {
+            println!("cargo:rustc-link-search=native={}", m5_lib_dir);
+            println!("cargo:rerun-if-env-changed=GEM5_M5_LIB_DIR");
+
+            if let Ok(m5_include) = env::var("GEM5_M5_INCLUDE") {
+                PathBuf::from(m5_include)
+            } else {
+                let guess = PathBuf::from(m5_lib_dir).join("..").join("include");
+
+                println!(
+                    "cargo:warning=GEM5_M5_INCLUDE not set. Guessing {}",
+                    guess.display()
+                );
+
+                guess
+            }
+        }
         Err(_) => {
-            let mut response =
-                ureq::get("https://github.com/gem5/gem5/archive/refs/tags/v25.1.0.0.tar.gz")
-                    .call()
-                    .unwrap();
+            println!("cargo:warning=GEM5_M5_LIB_DIR is not set.");
 
-            assert!(response.status().is_success());
+            if env::var("GEM5_M5_BUILD").is_err() {
+                println!("cargo:warning=GEM5_M5_BUILD not set. Guessing include path /usr/include");
 
-            let mut archive = Archive::new(GzDecoder::new(response.body_mut().as_reader()));
+                PathBuf::from("/usr/include")
+            } else {
+                let gem5_src_dir = match env::var("GEM5_SRC") {
+                    Ok(path) => PathBuf::from(path),
+                    Err(_) => {
+                        println!("cargo:warning=GEM5_SRC not set. Downloading release tar ball.");
 
-            archive.unpack(&out_path).unwrap();
+                        let mut response = ureq::get(format!(
+                            "https://github.com/gem5/gem5/archive/refs/tags/v{GEM5_VERSION}.tar.gz"
+                        ))
+                        .call()
+                        .unwrap();
 
-            out_path.join("gem5-25.1.0.0")
+                        assert!(response.status().is_success());
+
+                        let mut archive =
+                            Archive::new(GzDecoder::new(response.body_mut().as_reader()));
+
+                        archive.unpack(&out_path).unwrap();
+                        out_path.join(format!("gem5-{GEM5_VERSION}"))
+                    }
+                };
+                let m5_src_path = gem5_src_dir.join("util").join("m5").join("src");
+                let gem5_include_dir = gem5_src_dir.join("include");
+                let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+                let arch_map = [("riscv64", "riscv"), ("x86_64", "x86")]
+                    .into_iter()
+                    .collect::<HashMap<_, _>>();
+                let mut call_types = [
+                    ("inst", CallType::new("inst")),
+                    ("addr", CallType::new("addr")),
+                    ("semi", CallType::new("semi")),
+                ]
+                .into_iter()
+                .collect::<HashMap<_, _>>();
+                let abi = arch_map
+                    .get(arch.as_str())
+                    .unwrap_or(&arch.as_str())
+                    .to_string();
+                let abi_call_types_map = [
+                    (
+                        "riscv",
+                        Box::new(|call_types: &mut HashMap<&str, CallType>| {
+                            let call_type = call_types.get_mut("inst").unwrap();
+
+                            call_type.impl_file = Some("m5op.S");
+                            call_type.verifier = Some("verify_inst.cc");
+                            call_type.enabled = true;
+                            call_type.default = true;
+                        }) as Box<dyn Fn(_)>,
+                    ),
+                    (
+                        "x86",
+                        Box::new(|cts: &mut HashMap<&str, CallType>| {
+                            let ct = cts.get_mut("inst").unwrap();
+
+                            ct.impl_file = Some("m5op.S");
+                            ct.verifier = Some("verify_inst.cc");
+                            ct.enabled = true;
+
+                            let ct = cts.get_mut("addr").unwrap();
+
+                            ct.impl_file = Some("m5op_addr.S");
+                            ct.enabled = true;
+                            ct.default = true;
+                        }) as Box<dyn Fn(_)>,
+                    ),
+                ]
+                .into_iter()
+                .collect::<HashMap<_, _>>();
+
+                abi_call_types_map.get(abi.as_str()).unwrap()(&mut call_types);
+                drop(abi_call_types_map);
+
+                let call_types: Vec<_> = call_types
+                    .values()
+                    .filter(|call_type| call_type.enabled)
+                    .collect();
+                let m5ops = call_types
+                    .iter()
+                    .map(|call_type| {
+                        (
+                            call_type.name,
+                            PathBuf::from("abi")
+                                .join(&abi)
+                                .join(call_type.impl_file.unwrap()),
+                        )
+                    })
+                    .collect::<HashMap<_, _>>();
+                let all_m5ops = m5ops.values().collect::<Vec<_>>();
+                let m5_mmap = "m5_mmap.c";
+                let mut all_files = all_m5ops
+                    .into_iter()
+                    .map(|p| m5_src_path.join(p))
+                    .collect::<Vec<_>>();
+
+                all_files.push(m5_src_path.join(m5_mmap));
+
+                let mut cc_build = cc::Build::new();
+
+                if let Ok(m5_cc) = env::var("GEM5_M5_CC") {
+                    cc_build.compiler(m5_cc);
+                }
+
+                cc_build
+                    .include(&gem5_include_dir)
+                    .files(&all_files)
+                    .compile("m5");
+
+                gem5_include_dir
+            }
         }
     };
-    let m5_src_path = gem5_dir.join("util").join("m5").join("src");
-    let gem5_include_dir = gem5_dir.join("include");
-    let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    let arch_map = [("riscv64", "riscv"), ("x86_64", "x86")]
-        .into_iter()
-        .collect::<HashMap<_, _>>();
-    let mut call_types = [
-        ("inst", CallType::new("inst")),
-        ("addr", CallType::new("addr")),
-        ("semi", CallType::new("semi")),
-    ]
-    .into_iter()
-    .collect::<HashMap<_, _>>();
-    let abi = arch_map
-        .get(arch.as_str())
-        .unwrap_or(&arch.as_str())
-        .to_string();
-    let abi_call_types_map = [
-        (
-            "riscv",
-            Box::new(|call_types: &mut HashMap<&str, CallType>| {
-                let call_type = call_types.get_mut("inst").unwrap();
-
-                call_type.impl_file = Some("m5op.S");
-                call_type.verifier = Some("verify_inst.cc");
-                call_type.enabled = true;
-                call_type.default = true;
-            }) as Box<dyn Fn(_)>,
-        ),
-        (
-            "x86",
-            Box::new(|cts: &mut HashMap<&str, CallType>| {
-                let ct = cts.get_mut("inst").unwrap();
-
-                ct.impl_file = Some("m5op.S");
-                ct.verifier = Some("verify_inst.cc");
-                ct.enabled = true;
-
-                let ct = cts.get_mut("addr").unwrap();
-
-                ct.impl_file = Some("m5op_addr.S");
-                ct.enabled = true;
-                ct.default = true;
-            }) as Box<dyn Fn(_)>,
-        ),
-    ]
-    .into_iter()
-    .collect::<HashMap<_, _>>();
-
-    abi_call_types_map.get(abi.as_str()).unwrap()(&mut call_types);
-    drop(abi_call_types_map);
-
-    let call_types: Vec<_> = call_types
-        .values()
-        .filter(|call_type| call_type.enabled)
-        .collect();
-    let m5ops = call_types
-        .iter()
-        .map(|call_type| {
-            (
-                call_type.name,
-                PathBuf::from("abi")
-                    .join(&abi)
-                    .join(call_type.impl_file.unwrap()),
-            )
-        })
-        .collect::<HashMap<_, _>>();
-    let all_m5ops = m5ops.values().collect::<Vec<_>>();
-    let m5_mmap = "m5_mmap.c";
-    let mut all_files = all_m5ops
-        .into_iter()
-        .map(|p| m5_src_path.join(p))
-        .collect::<Vec<_>>();
-
-    all_files.push(m5_src_path.join(m5_mmap));
-
-    let mut cc_build = cc::Build::new();
-
-    if let Ok(m5_cc) = env::var("M5_CC") {
-        cc_build.compiler(m5_cc);
-    }
-
-    cc_build
-        .include(&gem5_include_dir)
-        .files(&all_files)
-        .compile("m5");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg(format!("-I{}/include", gem5_dir.display()))
+        .clang_arg(format!("-I{}", include.display()))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("unable to generate bindings");
